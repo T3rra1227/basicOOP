0543c117724c74d5465ad769e3940d5865a2b1ef
T3rra1227 <q15sbaig@wgsf.net>
2024-10-02 13:00:05 +0100

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -11,18 +11,26 @@ class Ball:
         self.xPos = x
         self.yPos = y
         self.colour = colour
-        self.ySpeed = 10
+        self.ySpeed = -10
         self.xSpeed = 8
     
-    def move(self):
+    def move(self, lives):
         self.yPos += self.ySpeed
         self.xPos += self.xSpeed
-        if self.yPos > 900 or self.yPos < 0:
+        if self.yPos < 0:
             self.ySpeed *= -1
+        if self.yPos > 900:
+            lives -= 1
+            self.ySpeed *= -1
+            self.yPos = 700
+            self.xPos = 500
         if self.xPos > 1000 or self.xPos < 0:
             self.xSpeed *= -1
             #xPos = 1000
+        
         drawEllipse(self.xPos, self.yPos, 20, 20, self.colour)
+        
+        return lives
         
 class Brick():
     def __init__(self,x,y, colour):
@@ -32,19 +40,53 @@ class Brick():
         self.colour = colour
         self.width = 75
         self.height = 50
+        self.active = True
         
     def draw(self):
-        drawRect(self.xPos,self.yPos,self.width, self.height, self.colour)
+        if self.active:
+            drawRect(self.xPos,self.yPos,self.width, self.height, self.colour)
         
-
-xPos = 500
-yPos = 0
-ySpeed = 0
-xSpeed = random.randint(-10,10)
-nBalls = random.randint(1,1000)
+    def detectHit(self,balls):
+        for ball in balls:
+            if self.xPos <= ball.xPos <= self.xPos + self.width:
+                if self.yPos <= ball.yPos <= self.yPos + self.height:
+                    ball.ySpeed *= -1
+                    return True
+        return False
+    
+    def update(self,balls):
+        self.draw()
+        if self.active and self.detectHit(balls):
+            self.active = False
+        
+class Bat(Brick): # inherits the Brick class: all the traits brick has, bat now has - appears on exams
+    def __init__(self,x,y,colour):
+        super().__init__(x,y,colour)
+        self.width = 150
+        self.height = 25
+        
+    def update(self,balls): #overriding brick data
+        self.draw()
+        self.detectHit(balls)
+        if keyPressed("right"):
+            self.xPos += 10
+        if keyPressed("left"):
+            self.xPos -= 10
+    
+        if self.xPos > 1150:
+            self.xPos = 0
+        if self.xPos < 0:
+            self.xPos = 110
+        
+class MultiBrick(Brick):
+    def update(self,balls):
+        self.draw()
+        if self.active and self.detectHit(balls):
+            self.active = False
+            balls.append( Ball(self.xPos, self.yPos, "yellow"))
 
 colours = ["red","orange","purple","blue","green"]
-balls = [ Ball(random.randint(50,850),0,random.choice(colours)) for i in range(1) ]
+balls = [ Ball(500,500,random.choice(colours)) for i in range(1) ]
 bricks = []
 
 for x in range(0,1000,77):
@@ -52,12 +94,29 @@ for x in range(0,1000,77):
     bricks.append(Brick(x,200,"red"))
     bricks.append(Brick(x,300,"blue"))
 
-while True:
+randomPos = random.randint(0,len(bricks))
+removedBrick = bricks.pop(randomPos)
+bricks.insert(randomPos,MultiBrick(removedBrick.xPos,removedBrick.yPos,"black"))
+
+
+bat = Bat(500,850,"lightblue") # Instantiation
+lives = 3
+livesLabel = makeLabel("Lives:", 24, 10, 10, "white")
+showLabel(livesLabel)
+gameOverLabel = makeLabel("GAME OVER", 80, 300, 450,"red")
+
+running = True
+while running:
     clearShapes()
+    changeLabel(livesLabel, "Lives: " + str(lives))
+    bat.update(balls)
     for b in balls:
-        b.move()
+        lives = b.move(lives)
+        if lives <= 0:
+            showLabel(gameOverLabel)
+            running = False
     for brick in bricks:
-        brick.draw()
+        brick.update(balls)
     tick(60)
     updateDisplay()
 
